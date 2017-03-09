a7512f538b5fe37225dd4eb2fb18ae13fff17789
Jody Lipkin <lipkinjody@gmail.com>
2017-03-08 21:01:52 -0800

    created 3 views and routes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,20 @@
 class GamesController < ApplicationController
+
+	def name_in_all_caps
+		@name = params[:name]
+		puts "*"*100
+		puts @name
+		puts "*"*100
+	end
+
+	def first_letter
+		@name = params[:name]
+	end
+
+	def guess_a_number
+		@guess = params[:guess].to_f
+	end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+	get "/name_in_all_caps" => "games#name_in_all_caps"
+	get "/first_letter" => "games#first_letter"
+	get "/guess_a_number/:guess" => "games#guess_a_number"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
